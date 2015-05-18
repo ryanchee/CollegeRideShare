c007bbf3e94186541e63aee870790f5e2973496e
ryanchee <ryancchee@yahoo.com>
2015-05-17 21:00:42 -0700

    added all view controllers for vertical prototype

---
+++ CollegeRideShare/DetailTripViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailTripViewController.swift
+//  CollegeRideShare
+//
+//  Created by Ryan Chee on 5/17/15.
+//  Copyright (c) 2015 Ryan Chee. All rights reserved.
+//
+
+import UIKit
+
+class DetailTripViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
